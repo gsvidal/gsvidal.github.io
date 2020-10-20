666535787975affb1d9eb9dd3587e9eaf16ffe14
Gonzo <gvidal.industrial@gmail.com>
2020-10-20 12:00:17 -0500

    Terminando últimos detalles de la versión mobile

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-function isPangram(string){
+
+const bodyBGColor = document.getElementById("body");
+
+const switch_button_container = document.getElementById("switch_button_container");
+
+const switch_button = document.getElementById("switch_button");
+
+const logo = document.querySelector(".logo-image");
+// const pName = document.querySelector(".project_name")
+// const olderProjects = document.query(".older-projects")
+
+
+var i;
+var olderProjects = document.querySelectorAll(".older-projects")
+var pName = document.querySelectorAll(".project_name")
+
+
+switch_button.addEventListener("click",light_dark_mode);
+
+function light_dark_mode() {
+  bodyBGColor.classList.toggle("light-dark-mode") 
   
+  switch_button.classList.toggle("light-dark-mode");
+
+  switch_button_container.classList.toggle("light-dark-mode");
+
+  logo.classList.toggle("light-dark-mode");
+
+  // pName.classList.toggle("light-dark-mode")
+
+  for(i=0; i<olderProjects.length; i++){
+    olderProjects[i].classList.toggle("light-dark-mode");
+  }
+  for(i=0; i<pName.length; i++){
+    pName[i].classList.toggle("light-dark-mode");
+  }
+
+
 }
-
-isPangram("The quick brown fox jumps over the lazy dog");
-
-var str = "The quick brown fox jumps over the lazy dog";
-str.sort();
-
-var str2 = [1.25,6,4,544,-2];
-str2.sort((a,b)=>a-b);
-
