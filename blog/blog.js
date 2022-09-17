b0a2b1e17bd04d5e553933443f54eabc60b9660f
Gonzo <gonzalovidal.dev@gmail.com>
2022-09-17 12:15:18 -0500

    Add new article to JS section - TDD

--- blog/blog.js
+++ blog/blog.js
@@ -1,11 +1,11 @@
-const topicsTitle = document.querySelectorAll(".topics-title");
-const topicsInfo = document.querySelectorAll(".topics-info");
+const topicsTitle = document.querySelectorAll('.topics-title');
+const topicsInfo = document.querySelectorAll('.topics-info');
 
-for(let i = 0; i < topicsTitle.length; i++){
-  topicsTitle[i].addEventListener("click", function () {
-  if(topicsTitle[i].classList.contains("topics-title-ready")) {
-    topicsInfo[i].classList.toggle("showHide");
-  }
+for (let i = 0; i < topicsTitle.length; i++) {
+  topicsTitle[i].addEventListener('click', function () {
+    if (topicsTitle[i].classList.contains('topics-title--ready')) {
+      topicsInfo[i].classList.toggle('showHide');
+    }
   });
 }
 
