572a67046b2ef501ce14d27ae7be88f7975a4ad3
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-04-09 01:28:30 -0500

    Add an intersection observer for sections to make a reveal effect when needed, also make a infinite autoplay carousel effect for certificate icons and add another google font to portfolio

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const bodyBGColor = document.getElementById('body');
-const switchButtonContainer = document.querySelector('.switch_button_container');
+const switchButtonContainer = document.querySelector(
+  '.switch_button_container'
+);
 const switchButton = document.querySelector('.switch_button');
 const logo = document.querySelector('.logo__bg');
 const linkPortfolioReact = document.querySelector('.welcome__portfolio-react');
@@ -7,12 +9,17 @@ const personalBlog = document.querySelector('.blog__link');
 const olderProjects = document.querySelectorAll('.older-projects');
 const olderProjectsImages = document.querySelectorAll('.older-projects-images');
 const projectNames = document.querySelectorAll('.project_name');
+const sectionsToReveal = document.querySelectorAll('.reveal');
+const buttonUp = document.querySelector('.button-up-container');
+const certificate = document.querySelector('.certificates');
+const certificateSlider = document.querySelector('.certificates__slider');
 
 //Titles changing color when dark-light mode turns
 const titles = document.querySelectorAll('.title-projects');
 
 switchButtonContainer.addEventListener('click', lightDarkMode);
 
+// lightDarkMode toggle dark and light mode on each element when is clicked
 function lightDarkMode() {
   bodyBGColor.classList.toggle('light-dark-mode');
   switchButton.classList.toggle('light-dark-mode');
@@ -21,13 +28,21 @@ function lightDarkMode() {
   linkPortfolioReact.classList.toggle('light-dark-mode');
   personalBlog.classList.toggle('light-dark-mode');
 
-  olderProjects.forEach((olderProject) => olderProject.classList.toggle('light-dark-mode'));
+  olderProjects.forEach((olderProject) =>
+    olderProject.classList.toggle('light-dark-mode')
+  );
 
-  olderProjectsImages.forEach((olderProjectsImage) => olderProjectsImage.classList.toggle('light-dark-mode'));
+  olderProjectsImages.forEach((olderProjectsImage) =>
+    olderProjectsImage.classList.toggle('light-dark-mode')
+  );
 
-  projectNames.forEach((projectName) => projectName.classList.toggle('light-dark-mode'));
+  projectNames.forEach((projectName) =>
+    projectName.classList.toggle('light-dark-mode')
+  );
 
   titles.forEach((title) => title.classList.toggle('light-dark-mode'));
+
+  certificateSlider.classList.toggle('light-dark-mode');
 }
 
 //Web Technologies - Logo Animations
@@ -37,7 +52,9 @@ let lastIndex;
 function fadeRandomIcon() {
   let randomIconIndex = Math.floor(icons.length * Math.random());
   if (lastIndex === randomIconIndex) {
-    randomIconIndex !== icons.length - 1 ? randomIconIndex++ : randomIconIndex--;
+    randomIconIndex !== icons.length - 1
+      ? randomIconIndex++
+      : randomIconIndex--;
   }
   icons[lastIndex]?.classList.remove('toolbox__fade');
   icons[randomIconIndex].classList.add('toolbox__fade');
@@ -55,7 +72,8 @@ const imagesShadow = document.querySelectorAll('.project_image');
 imagesShadow.forEach((image) => {
   image.addEventListener('mouseover', (event) => {
     if (!image.parentElement.classList.contains('p-none')) {
-      image.parentElement.style.boxShadow = '0px 0px 35px 2px #b8a5f2, 0px 0px 15px 2px #ffffff';
+      image.parentElement.style.boxShadow =
+        '0px 0px 35px 2px #b8a5f2, 0px 0px 15px 2px #ffffff';
     } else {
       image.style.transform = 'none';
     }
@@ -67,7 +85,7 @@ imagesShadow.forEach((image) => {
   });
 });
 
-// Live/source
+// Live/repo
 const images = document.querySelectorAll('.project_image-container');
 const githubIcon = document.querySelector('.project__github-image');
 
@@ -82,3 +100,40 @@ images.forEach((image) =>
     image?.parentNode.nextElementSibling.classList.remove('show');
   })
 );
+
+const scrollToTop = (event) => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+buttonUp.addEventListener('click', scrollToTop);
+
+// Sections to reveal when appear in viewport
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Element is in viewport
+      entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active');
+    }
+  });
+});
+
+sectionsToReveal.forEach((element) => {
+  observer.observe(element);
+});
+
+window.addEventListener('scroll', () => {
+  var elementTop = certificate.getBoundingClientRect().top;
+
+  if (elementTop < 0) {
+    buttonUp.classList.add('revealed');
+  } else {
+    buttonUp.classList.remove('revealed');
+  }
+});
+
+certificateSlider.addEventListener('click', lightDarkMode);
